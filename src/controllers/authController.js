07483e3c82d24d440332bfd395c5b7de14dc01de
Yeva Krysheminska <yevaluation@gmail.com>
2026-04-19 15:58:16 +0300

    hw5fixf

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -126,7 +126,7 @@ export const requestResetEmail = async (req, res) => {
 
   try {
     await sendEmail({
-      from: process.env.SMTP_USER,
+      from: process.env.SMTP_FROM,
       to: email,
       subject: "Reset password",
       html,
